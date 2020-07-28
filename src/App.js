aac66b29d1ca02e1a236febfa209720eb6012bed
Marcin <m.kedziera@gmail.com>
2020-07-28 18:58:46 +0100

    amarfucker

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <p>
-            Hello Beeper!
+            Hello Fucker Amar!
           </p>
           <a
             className="App-link"
