32e5210b7c13c7ca7ea28b9d4c2b53d7caf5a4b8
Marcin <m.kedziera@gmail.com>
2020-08-31 16:01:38 +0100

    lets try this

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <div className="nav-wrap">
+        <div className="nav">
+          <ul className="wsite-menu-default">
+            <li id="active" class="wsite-menu-item-wrap">
+              <a href="#" className="wsite-menu-item-wrap wsite-nav-1" style="position:relative;">
+                
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
